0224c397936faaded94936f09fe4c7f684752910
Alex Simisi <simisinta.officiel@gmail.com>
2019-06-19 14:36:14 +0200

    ajout du php scniffer

--- public/index.php
+++ public/index.php
@@ -2,15 +2,19 @@
 // Récupération de l'autoloader créé par composer
 require dirname(__DIR__) . '/vendor/autoload.php';
 // Les "use" des différentes classes
+use DI\ContainerBuilder;
 use Slim\App;
+
 // Config
 $config = require dirname(__DIR__) . '/config/config.php';
-// On créé l'application Slim
-$app = new App($config);
 
 // recuperation du conteneur
-$config = require dirname(__DIR__) . '/config/container.php';
+$builder = new DI\ContainerBuilder();
+$builder->addDefinitions(dirname(__DIR__) . '/config/container.php');
+$container = $builder->build();
 
+// On créé l'application Slim
+$app = new App($container);
 // recuperation de la route
 require dirname(__DIR__) . '/config/routes.php';
 // Renvoi de la réponse au navigateur
